b7370408a400a844cc24ca7f49d4e7f1c4df371d
Tarrissarh <tarrissarh@gmail.com>
2020-03-07 21:05:39 +0300

    Create entitiesm database schema, add easy_admin package

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -42,6 +42,11 @@ class Comment
      */
     private $conference;
 
+    public function __toString(): string
+    {
+        return (string) $this->getEmail();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
