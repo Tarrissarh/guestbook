33e6041b1888d1af4ebbd913c276ee876d16cf3a
Tarrissarh <tarrissarh@gmail.com>
2020-04-05 22:20:21 +0300

    fix test

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -3,16 +3,56 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Panther\PantherTestCase;
 
-class ConferenceControllerTest extends WebTestCase
+class ConferenceControllerTest extends PantherTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        //$client = static::createClient();
+        $client = static::createPantherClient(['external_base_uri' => $_SERVER['SYMFONY_DEFAULT_ROUTE_URL']]);
 
         $client->request('GET', '/');
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h2', 'Give your feedback');
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('h2', 'Give your feedback');
+    }
+
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/conference/amsterdam-2019');
+
+        $client->submitForm(
+            'Submit',
+            [
+                'comment_form[author]' => 'Fabien',
+                'comment_form[text]'   => 'Some feedback from an automated functional test',
+                'comment_form[email]'  => 'me@automat.ed',
+                'comment_form[photo]'  => dirname(__DIR__, 2) . '/public/images/under-construction.gif',
+            ]
+        );
+
+        self::assertResponseRedirects();
+
+        $client->followRedirect();
+
+        self::assertSelectorExists('div:contains("There are 2 comments")');
+    }
+
+    public function testConferencePage()
+    {
+        $client  = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+
+        self::assertPageTitleContains('Amsterdam');
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('h2', 'Amsterdam 2019');
+        self::assertSelectorExists('div:contains("There are 1 comments")');
     }
 }
